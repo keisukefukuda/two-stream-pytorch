0c9a557d0e78bc901ef95229f563daf151b6cf15
Keisuke Fukuda <keisukefukuda@gmail.com>
2018-03-13 18:41:21 +0900

    minor bugfixes

--- datasets/build_of.py
+++ datasets/build_of.py
@@ -25,7 +25,8 @@ def run_optical_flow(vid_item):
     flow_x_path = '{}/flow_x'.format(out_full_path)
     flow_y_path = '{}/flow_y'.format(out_full_path)
 
-    cmd = os.path.join(df_path + 'build/extract_gpu')+' -f {} -x {} -y {} -i {} -b 20 -t 1 -d {} -s 1 -o {} -w {} -h {}'.format(
+    bin = os.path.join(df_path, 'build', 'extract_gpu')
+    cmd = bin + ' -f={} -x={} -y={} -i={} -b=20 -t=1 -d={} -s=1 -o={} -w={} -h={}'.format(
         quote(vid_path), quote(flow_x_path), quote(flow_y_path), quote(image_path), dev_id, out_format, new_size[0], new_size[1])
 
     os.system(cmd)
@@ -65,11 +66,11 @@ if __name__ == '__main__':
     NUM_GPU = args.num_gpu
 
     if not os.path.isdir(out_path):
-        print("creating folder: "+out_path)
+        print("creating folder: {}".format(out_path))
         os.makedirs(out_path)
 
-    vid_list = glob.glob(src_path+'/*/*.'+ext)
+    vid_list = glob.glob(src_path + '/*/*.' + ext)
     print(len(vid_list))
     pool = Pool(num_worker)
-    pool.map(run_optical_flow, zip(vid_list, xrange(len(vid_list))))
+    pool.map(run_optical_flow, zip(vid_list, range(len(vid_list))))
 
